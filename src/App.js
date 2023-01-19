4569a4862e91ad09fb96c4f13771e8bafba68ef5
Nikhil <nikhilsindiri3@gmail.com>
2023-01-19 08:50:37 +0530

    add getreq,states in http req,error handler

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies,setMovies] = useState([])
+  const [isloading,setIsLoading] = useState(false);
+  const [error,setError] = useState(null);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false)
+    } catch (err) {
+      setError(err.message)
+    }
+    setIsLoading(false)
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isloading && movies.length>0 && <MoviesList movies={movies} />}
+        {!isloading && movies.length === 0 && !error && <p> found no movies </p>}
+        {!isloading && error && <p>{error}</p>}
+        {isloading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
